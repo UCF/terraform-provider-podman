6a0080992cbf6625e5292b9fc44b49a989e2271e
Chandra <chandra.kethireddy@ucf.edu>
2024-11-15 13:10:56 -0500

    setup directory for work

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
+	"github.com/hashicorp/terraform-provider-podman/internal/podman"
 )
 
 var (
@@ -31,11 +31,11 @@ func main() {
 		// TODO: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "registry.terraform.io/UCF/terraform-provider-podman",
 		Debug:   debug,
 	}
 
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	err := providerserver.Serve(context.Background(), podman.Provider(), opts)
 
 	if err != nil {
 		log.Fatal(err.Error())
